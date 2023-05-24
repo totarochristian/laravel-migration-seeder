ea644e2c0fc27292618a484ca75321be1e5f88ce
totarochristian <totaro.christian.18@gmail.com>
2023-05-24 14:24:16 +0200

    Add the fields of the table in the trains create migration

--- database/migrations/2023_05_24_121448_create_trains_table.php
+++ database/migrations/2023_05_24_121448_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company',100);
+            $table->string('departure_station',100);
+            $table->string('arrival_station',100);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code');
+            $table->tinyInteger('carriage_number')->unsigned()->default(0);
+            $table->boolean('in_time')->default(true);
+            $table->boolean('deleted')->default(false);
             $table->timestamps();
         });
     }
