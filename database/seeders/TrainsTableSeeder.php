592b7e75c509fd92d0ff337af3badc38e8ae3edd
totarochristian <totaro.christian.18@gmail.com>
2023-05-24 14:42:18 +0200

    Update trains table seeder run method

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\trains;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -12,8 +14,18 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i=0; $i<10; $i++){
+            $train = new trains();
+            $train->company = $faker->company();
+            $train->departure_station = $faker->city();
+            $train->arrival_station = $faker->city();
+            $train->departure_time = $faker->dateTime();
+            $train->arrival_time = $faker->dateTimeInInterval($train->departure_time,"+2 days");
+            $train->train_code = $faker->realText(2).$faker->numberBetween(1000,9999).$faker->realText(2);
+            $train->carriage_number = $faker->numberBetween(0,3);
+            $train->save();
+        }
     }
 }
